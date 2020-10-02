c1f982751e815e651a85c596aa1ecd16d72d9889
huhrichard <richardpokard@gmail.com>
2020-10-02 19:03:52 -0400

    no message

---
+++ submit_exposure_analyzer.py
@@ -0,0 +1,17 @@
+import sys
+import os
+
+suffix = sys.argv[-1]
+
+lsf_str = "#!/bin/bash\n#BSUB -J asthma_project\n#BSUB -P acc_pandeg01a\n#BSUB -q premium\n" \
+          "#BSUB -n 5\n#BSUB -W 48:00\n#BSUB -o analyzer_%J.stdout\n#BSUB -eo analyzer_%J.stderr\n" \
+          "#BSUB -R rusage[mem=10000]\nmodule purge\n"
+python_cmd = "python exposure_analyzer.py {}".format(suffix)
+
+lsf_name = "asthma.lsf"
+script = open(lsf_name, 'w')
+script.write(lsf_str)
+script.write(python_cmd)
+script.close()
+os.system("bsub < {}".format(lsf_name))
+os.remove(lsf_name)
