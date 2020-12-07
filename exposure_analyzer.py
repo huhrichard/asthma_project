b89594f640dfeb1ca440104bb5dabf82f8959e31
huhrichard <richardpokard@gmail.com>
2020-12-07 12:54:19 -0500

    no message

--- exposure_analyzer.py
+++ exposure_analyzer.py
@@ -1126,8 +1126,8 @@ if __name__ == "__main__":
     if not os.path.exists(m_Dir):
         os.mkdir(m_Dir)
     # plotDir = os.path.join(m_Dir, 'nata_{}_{}'.format(yr_name, yr_f))
-    if not os.path.exists(plotDir):
-        os.mkdir(plotDir)
+    # if not os.path.exists(plotDir):
+    #     os.mkdir(plotDir)
     pvalue_df = pd.DataFrame(columns=['profile', 'outcome', 'p_val', 'relation',
                                       'coef', 'coef_95CI_lower', 'coef_95CI_upper','freq', 'pos_count', 'neg_count', 'binary_outcome',
                                       'max_count'
